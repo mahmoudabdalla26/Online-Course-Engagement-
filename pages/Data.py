5e63827c579f54aeb9f6e798c0cf9dbe0cabe7dd
Mahmoud Abdalla <112134663+mahmoudabdalla26@users.noreply.github.com>
2024-09-24 21:28:30 +0300

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -9,12 +9,12 @@ st.markdown("<center><h1> Online Courses Dataset </h1></center>", unsafe_allow_h
 st.write('Online Courses Dataset Before Cleaning And Preprocessing:')
 
 # Reading and displaying the dataset before cleaning and preprocessing
-df_before = pd.read_csv(r"C:\Users\Lenovo\Desktop\Eslam_Final_Project\Sourse\online_course_engagement_data.csv")
+df_before = pd.read_csv("Sourse/online_course_engagement_data.csv")
 st.write(df_before)
 
 # Displaying information about the dataset after cleaning and preprocessing
 st.write('Online Courses Dataset After Cleaning And Preprocessing:')
 
 # Reading and displaying the cleaned dataset
-df_after = pd.read_csv(r"C:\Users\Lenovo\Desktop\Eslam_Final_Project\Sourse\cleaned_data.csv")
+df_after = pd.read_csv("Sourse/cleaned_data.csv")
 st.write(df_after)
